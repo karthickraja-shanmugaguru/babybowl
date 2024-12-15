5950d3d8aef113d3c3e134aa5dfe3f70e3ebba37
Karthickraja S <karthick.raja@justact.co.in>
2024-12-15 19:32:04 +0530

    remove banner 1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 
 # Mock Data
-banner_data = [,
+banner_data = [
     {"id": "banner2", "image_url": "https://backend.babybowl.life/static/Image/Banner2.png", "banner_type": "Home"},
     {"id": "banner3", "image_url": "https://backend.babybowl.life/static/Image/Banner3.png", "banner_type": "Home"}
 ]
